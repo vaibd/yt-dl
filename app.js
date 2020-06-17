ebc842c06a00301bfa21b6f0aec9190292d75041
vaibd <vasud1515gmail.com>
2020-06-17 16:17:37 +0530

    port

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ app.post("/login", function(req, res){
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000.");
 });
 
